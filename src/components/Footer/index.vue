1d88ff681be7e42c494d0ef8e250231d0acbe970
Higor Rodrigues <higor@planmkt.com.br>
2024-07-25 11:49:24 -0300

    fix: adicionado link no footer

--- src/components/Footer/index.vue
+++ src/components/Footer/index.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex items-center justify-center px-4 py-2 w-full bg-blue-900">
-        <h2 class="text-white">FOOTER</h2>
+    <div class="flex items-center justify-center px-4 py-2 w-full bg-blue-900 gap-1">
+        <h2 class="text-white">Feito por </h2> 
+        <a href="https://www.instagram.com/higornmr/" class="text-white pointer"> Higor Rodrigues</a>
     </div>
 </template>
 
